6438224d075fdbec76bbb34c9433053ab0fb44ca
Apexa Jodhani <apexa.bvminfotech@gmail.com>
2023-05-30 19:22:53 +0530

    Add migration

---
+++ database/migrations/2023_05_29_135624_create_staff_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('staff', function (Blueprint $table) {
+            $table->id();
+            $table->string('staff_name');
+            $table->string('staff_phone');
+            $table->string('course_id');
+            $table->string('employee_ID');
+            $table->boolean('staff_I_card')->default(0);
+            $table->boolean('staff_uniform')->default(0);
+            $table->string('staff_address');
+            $table->string('eme_phone')->comment('Emergency contact no');
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('staff');
+    }
+};
