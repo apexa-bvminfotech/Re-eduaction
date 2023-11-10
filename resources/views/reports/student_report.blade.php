4bfac360d297779718372fec741114ced7152d1f
bhagvatiitaliya <bhagvatiitaliya001@gmail.com>
2023-11-10 16:05:11 +0530

    add report

--- resources/views/reports/student_report.blade.php
+++ resources/views/reports/student_report.blade.php
@@ -13,12 +13,44 @@
         </section>
         <section class="content">
             <div class="container-fluid">
-                <div class="card">
-                    <div class="card-header">
-                        <h3 class="card-title">Student Details</h3>
-                    </div>
-                    <div class="card-body">
-                        {{dd($students)}}
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+                            <!-- /.card-header -->
+                            <div class="card-body">
+                                <table id="example1" class="table table-bordered table-striped">
+                                    <thead>
+                                    <tr>
+                                        <th>No</th>
+                                        <th><span>Date</span></th>
+                                        <th><span>Student Name</span></th>
+                                        <th><span>Mobile Number</span></th>
+                                        <th><span>Course</span></th>
+                                        <th><span>Std</span></th>
+                                        <th><span>Medium</span></th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    @foreach($students as $key=>$student)
+                                        <tr>
+                                            <td>{{ $key+1 }}</td>
+                                            <td>{{$student->registration_date}}</td>
+                                            <td>{{ $student->name }} {{ $student->surname }}</td>
+                                            <td>{{ $student->father_contact_no }}</td>
+                                            <td>
+                                                @foreach($student->courses as $course)
+                                                    {{$course->course->course_name }}<br>
+                                                @endforeach
+                                            </td>
+                                            <td>{{ $student->standard }}</td>
+                                            <td>{{ $student->medium }}</td>
+                                        </tr>
+                                    @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                            <!-- /.card-body -->
+                        </div>
                     </div>
                 </div>
             </div>
